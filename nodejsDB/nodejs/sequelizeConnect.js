4e711992ef5ecad9958eff602da2e1fe154b22a2
andresarangoo <arango.ortiz.felipe@accenture.com>
2021-09-23 22:30:21 -0500

    Update sequelize...........

--- nodejsDB/nodejs/sequelizeConnect.js
+++ nodejsDB/nodejs/sequelizeConnect.js
@@ -1,30 +1,32 @@
 'use strict';
-const { Sequelize } = require('sequelize');
+const {Sequelize} = require('sequelize');
 const AWS = require('aws-sdk');
 const secretsmanager = new AWS.SecretsManager();
 
-let { host, user, password, port, database, dialect };
+let info;
 
 secretsmanager.getSecretValue({ SecretId: 'ProductsSecretManager' }, function (err, data) {
     if (err) console.log(err, err.stack); 
     else {
-        const info = JSON.parse(data.SecretString);
-        host = info.host;
-        user = info.password;
-        password = info.username;
-        port = info.port;
-        database = info.dbname;
-        dialect = info.engine;
+        info = JSON.parse(data.SecretString);
     }
 });
 
-const sequelize = new Sequelize(database, user, password, {
+
+const host = info.host;
+const user = info.password;
+const password = info.username;
+const port = info.port;
+const database = info.dbname;
+const dialect = info.engine;
+
+const sequelize = new Sequelize (database, user, password, {
     host,
     port,
     dialect
 });
 
-sequelize.sync({ alter: true }).then(() => { console.log("listo") }).catch((error) => { console.error("Esto es el error", error) });
+sequelize.sync({alter: true}).then(()=>{console.log("listo")}).catch((error) =>{console.error("Esto es el error",error)});
 
 const db = {};
 
