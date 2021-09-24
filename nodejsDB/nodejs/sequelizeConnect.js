7a0467b40409cbc13a9007e4f2004861c185f178
andresarangoo <arango.ortiz.felipe@accenture.com>
2021-09-24 14:45:07 -0500

    Add load credentials...........

--- nodejsDB/nodejs/sequelizeConnect.js
+++ nodejsDB/nodejs/sequelizeConnect.js
@@ -1,24 +1,24 @@
 'use strict';
-const { Sequelize } = require('sequelize');
-const credentials = require('./credentials');
+const {Sequelize} = require('sequelize');
 
-let DBase = async () => {
-    const { host, user, password, port, database, dialect } = credentials.loadCredentials();
+const host = process.env.PRODUCTS_HOST;
+const user = process.env.PRODUCTS_USER;
+const password = process.env.PRODUCTS_PASSWORD;
+const port = process.env.PRODUCTS_PORT;
+const database = process.env.PRODUCTS_DB;
+const dialect = process.env.PRODUCTS_DIALECT;
 
-    const sequelize = new Sequelize(database, user, password, {
-        host,
-        port,
-        dialect
-    });
+const sequelize = new Sequelize (database, user, password, {
+    host,
+    port,
+    dialect
+});
 
-    sequelize.sync({ alter: true }).then(() => { console.log("listo") }).catch((error) => { console.error("Esto es el error", error) });
+sequelize.sync({alter: true}).then(()=>{console.log("listo")}).catch((error) =>{console.error("Esto es el error",error)});
 
-    const db = {};
+const db = {};
 
-    db.sequelize = sequelize;
-    db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-    return db;
-}
-
-module.exports = DBase;
+module.exports = db;
