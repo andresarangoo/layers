9a23efa8e566b18538cc2ea47d52b4503a04b88d
andresarangoo <arango.ortiz.felipe@accenture.com>
2021-09-27 18:17:36 -0500

    Update sequelize..

--- nodejsDB/nodejs/sequelizeConnect.js
+++ nodejsDB/nodejs/sequelizeConnect.js
@@ -12,9 +12,9 @@ module.exports.DBase = async () => {
         dialect
     });
 
-    sequelize.sync({ alter: true }).then(() => { console.log("listo") }).catch((error) => { console.error("Esto es el error", error) });
-    Order.sync();
-    Product.sync();
+    sequelize.sync().then(() => { console.log("listo") }).catch((error) => { console.error("Esto es el error", error) });
+    Order.sync().then(() => { console.log("listo 2") }).catch((error) => { console.error("Esto es el error 1", error) });
+    Product.sync().then(() => { console.log("listo 3") }).catch((error) => { console.error("Esto es el error 2", error) });
 
     const db = {
         sequelize : sequelize,
